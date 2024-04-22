1fadfc8682771bf2d76d0da699853761b8ca2a5d
danielmrrk <daniel.merkk@yahoo.com>
2024-04-22 14:24:39 +0200

    feat(Game): harvest implementation

--- src/main/scala/bohnanza/aview/Tui.scala
+++ src/main/scala/bohnanza/aview/Tui.scala
@@ -3,5 +3,15 @@ package bohnanza.aview
 import bohnanza.model.Game
 
 class Tui {
-  def processInputLine(input: String, game: Game): Game {}
+  def processInputLine(input: String, game: Game): Game = {
+    val splittedInput = input.split(" ")
+    splittedInput[0] match {
+        case "start"
+        case "next"
+        case "plant" => game.players[splittedInput[1]]
+        case "harvest"
+        case "cards"
+        case _
+    }
+  }
 }
--- src/main/scala/bohnanza/model/Game.scala
+++ src/main/scala/bohnanza/model/Game.scala
@@ -1,3 +1,15 @@
 package bohnanza.model
 
-class Game(players: Vector[Player], deck: Deck, gameField: GameField) {}
+class Game(players: Vector[Player], deck: Deck, gameField: GameField) {
+  def copy(
+      players: Vector[Player] = players,
+      deck: Deck = deck,
+      gameField: GameField = gameField
+  ): Game = Game(players, deck, gameField)
+
+  def harvestField(playerIndex: Int, beanFieldIndex: Int) = {
+    val updatedPlayer = players(playerIndex).harvestField(beanFieldIndex)
+    val updatedPlayers = players.updated(playerIndex, updatedPlayer)
+    copy(players = updatedPlayers)
+  }
+}
