aa498fe8672246bc5e98b896fb54d4d73404f674
danielmrrk <daniel.merkk@yahoo.com>
2024-04-25 22:52:24 +0200

    feat: implement test for turnOverField

--- src/test/scala/bohnanza/model/TurnOverFieldSpec.scala
+++ src/test/scala/bohnanza/model/TurnOverFieldSpec.scala
@@ -4,4 +4,26 @@ import org.scalatest.wordspec.AnyWordSpec
 
 import bohnanza.model.*
 
-class TurnOverFieldSpec extends AnyWordSpec with Matchers {}
+val emptyTurnOverField = TurnOverField(List.empty)
+val initialTurnOverField = TurnOverField(List(Bean.Firebean, Bean.Firebean))
+
+class TurnOverFieldSpec extends AnyWordSpec with Matchers {
+  "add a card to the turnOverField" when {
+    "card is None" in {
+      val updatedTurnOverField = emptyTurnOverField.addCardToTurnOverField(None)
+      emptyTurnOverField shouldBe updatedTurnOverField
+    }
+
+    "card is valid" in {
+      val updatedTurnOverField =
+        emptyTurnOverField.addCardToTurnOverField(Some(Bean.Firebean))
+      updatedTurnOverField.cards.size shouldBe emptyTurnOverField.cards.size + 1
+    }
+  }
+
+  "take a card from the turnOverField" in {
+    val (cardTaken, updatedTurnOverField) = initialTurnOverField.takeCard(0)
+    cardTaken shouldBe Bean.Firebean
+    updatedTurnOverField.cards.size shouldBe initialTurnOverField.cards.size - 1
+  }
+}
