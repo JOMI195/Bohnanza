760656a95e74fa444f7aced85835e10bbe9a3351
danielmrrk <140114492+danielmrrk@users.noreply.github.com>
2024-04-26 08:31:31 +0200

    implement main class models and TUI (#12) (#13)
    * feat: basic models
    
    * feat(Game): harvest implementation
    
    * Delete Main.scala
    
    * Update project
    
    * Update tui
    
    * Feat: tui command "draw"
    
    * feat: implement tui command harvest
    
    * delete println
    
    * feat: implement plantToField function
    
    * feat: implement in tui turn
    
    * feat: fix turn bug
    
    * feat: improve turn and take function, solve bug that when take turnOverField does not change
    
    * Feat: implement Tui command plant
    
    * feat: add test file structure
    
    * test: implement tests
    
    * some explanatory comments
    
    * feat: implement all TUI test + add TurnOverField take function + solve bugs related to tests
    
    * feat: implement the rest of the tests for beanField, gameSpec and player
    
    * feat: implement test for turnOverField
    
    * feat: implement missing beanField and Game tests
    + add additional bean for testing reasons
    
    ---------
    
    Co-authored-by: JOMI195 <johannes.middelberg@gmx.de>
    Co-authored-by: Johannes <57303615+JOMI195@users.noreply.github.com>

---
+++ src/main/scala/bohnanza/Bohnanza.scala
@@ -0,0 +1,35 @@
+package bohnanza
+import scala.io.StdIn.readLine
+
+import bohnanza.model.*
+import bohnanza.aview.*
+
+object Bohnanza {
+  val p1 = Player(
+    name = "Jomi",
+    coins = 0,
+    beanFields = List(BeanField(None)),
+    cards = List()
+  )
+  val p2 = Player(
+    name = "Daniel",
+    coins = 0,
+    beanFields = List(BeanField(Option(Bean.Firebean), 4)),
+    cards = List()
+  )
+  val d = Deck(cards = List(Bean.Firebean, Bean.Firebean, Bean.Firebean))
+  val t = TurnOverField(cards = List())
+
+  var game = Game(players = List(p1, p2), deck = d, turnOverField = t)
+  val tui = new Tui
+
+  def main(args: Array[String]): Unit = {
+    var input: String = ""
+
+    println("Starting new game...")
+    while (input != "exit") {
+      input = readLine()
+      game = tui.processInputLine(input, game)
+    }
+  }
+}
---
+++ src/test/scala/bohnanza/model/PlayerSpec.scala
@@ -0,0 +1,107 @@
+import org.scalatest._
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+import bohnanza.model.*
+
+class PlayerSpec extends AnyWordSpec with Matchers {
+  "Player" should {
+
+    "create a copy with updated attributes" in {
+      val originalPlayer = Player("Original", List.empty, 0, List.empty)
+
+      val updatedPlayer = originalPlayer.copy(
+        name = "Updated",
+        coins = 10,
+        beanFields = List(BeanField(None)),
+        cards = List(Bean.Firebean)
+      )
+
+      updatedPlayer.name should be("Updated")
+      updatedPlayer.coins should be(10)
+      updatedPlayer.cards should be(List(Bean.Firebean))
+      updatedPlayer.beanFields should be(List(BeanField(None)))
+    }
+
+    "add a card to hand" in {
+      val player = Player("TestPlayer", List.empty, 0, List.empty)
+      val cardToAdd = Bean.Firebean
+
+      val updatedPlayer = player.addCardToHand(cardToAdd)
+
+      updatedPlayer.cards should be(List(cardToAdd))
+    }
+
+    "pop a card from cards" when {
+      "cards list is not empty" in {
+        val initialCards = List(Bean.Firebean, Bean.Firebean, Bean.Firebean)
+        val player = Player("TestPlayer", List.empty, 0, initialCards)
+
+        val (poppedCard, updatedPlayer) = player.popCardFromCards()
+
+        poppedCard should be(Some(Bean.Firebean))
+        updatedPlayer.cards should be(List(Bean.Firebean, Bean.Firebean))
+      }
+
+      "cards list is empty" in {
+        val player = Player("TestPlayer", List.empty, 0, List.empty)
+
+        val (poppedCard, updatedPlayer) = player.popCardFromCards()
+
+        poppedCard should be(None)
+        updatedPlayer.cards should be(List.empty)
+      }
+    }
+
+    "plant a card from cards" when {
+      "cards list is not empty" in {
+        val initialCards = List(Bean.Firebean, Bean.Firebean, Bean.Firebean)
+        val initialPlayer =
+          Player("TestPlayer", List(BeanField(None)), 0, initialCards)
+
+        val updatedPlayer = initialPlayer.plantCardFromCards(0)
+
+        updatedPlayer.beanFields(0).bean should be(Some(Bean.Firebean))
+        updatedPlayer.cards should be(List(Bean.Firebean, Bean.Firebean))
+      }
+
+      "cards list is empty" in {
+        val player = Player("TestPlayer", List.empty, 0, List.empty)
+
+        val updatedPlayer = player.plantCardFromCards(0)
+
+        updatedPlayer.beanFields should be(List.empty)
+        updatedPlayer.cards should be(List.empty)
+      }
+    }
+
+    "harvest beanField at a specific index" in {
+      val initialCards = List.empty
+      val initialPlayer =
+        Player(
+          "TestPlayer",
+          List(BeanField(Option(Bean.Firebean), 4)),
+          0,
+          initialCards
+        )
+
+      val updatedPlayer = initialPlayer.harvestField(0)
+      updatedPlayer.beanFields.head.quantity shouldBe 0
+      updatedPlayer.coins shouldBe 2
+    }
+
+    "plant a bean to a specific beanField" in {
+      val initialCards = List.empty
+      val initialPlayer =
+        Player(
+          "TestPlayer",
+          List(BeanField(Option(Bean.Firebean), 3)),
+          0,
+          initialCards
+        )
+
+      val updatedPlayer = initialPlayer.plantToField(Bean.Firebean, 0)
+      updatedPlayer.beanFields.head.quantity shouldBe 4
+    }
+  }
+}
