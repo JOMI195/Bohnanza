24715767324686891602b9cdd5d2c534c4fa668f
JOMI195 <johannes.middelberg@gmx.de>
2024-06-02 14:21:59 +0200

    Fix Sus Gui GameLabel

--- src/main/scala/de/htwg/se/bohnanza/aview/gui/components/global/GameLabel.scala
+++ src/main/scala/de/htwg/se/bohnanza/aview/gui/components/global/GameLabel.scala
@@ -6,46 +6,10 @@ import scalafx.scene.text.Font
 
 class GameLabel(initialText: String, scalingFactor: Double = 1.0)
     extends Label {
-
+  val inititalFontSize = 20
   text = initialText
-
-  // Minimum and maximum font size
-  private val minFontSize: Double = 10
-  private val maxFontSize: Double = 100
-
-  // Properties for label size
-  private val labelWidth: DoubleProperty =
-    new DoubleProperty(this, "labelWidth", 0)
-  private val labelHeight: DoubleProperty =
-    new DoubleProperty(this, "labelHeight", 0)
-
-  // Add listeners to width and height properties
-  this.widthProperty().addListener { (_, _, newWidth) =>
-    labelWidth.set(newWidth.doubleValue())
-    adjustFontSize()
-  }
-
-  this.heightProperty().addListener { (_, _, newHeight) =>
-    labelHeight.set(newHeight.doubleValue())
-    adjustFontSize()
-  }
-
-  // Method to calculate and set the appropriate font size
-  private def adjustFontSize(): Unit = {
-    val newFontSize = calculateFontSize(labelWidth.value, labelHeight.value)
-    if (this.font.value != null) {
-      this.font = Font(this.font.value.getFamily, newFontSize)
-    } else {
-      this.font = Font(newFontSize) // Fallback to default font family
-    }
-    this.style = s"-fx-font-size: ${newFontSize}; -fx-text-fill: #7A2626;"
-  }
-
-  // Basic calculation of font size based on label size and scaling factor
-  private def calculateFontSize(width: Double, height: Double): Double = {
-    val sizeBasedOnWidth = (width / 10) * scalingFactor
-    val sizeBasedOnHeight = (height / 2) * scalingFactor
-    val newSize = Math.min(sizeBasedOnWidth, sizeBasedOnHeight)
-    Math.max(minFontSize, Math.min(newSize, maxFontSize))
-  }
+  style = s"-fx-font-size: ${inititalFontSize * scalingFactor};"
+  styleClass.add(
+    "game-label"
+  )
 }
